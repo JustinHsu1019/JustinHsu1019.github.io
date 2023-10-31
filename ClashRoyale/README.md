ec2a33298ca0e0e5ce0cbead16a0d9a72c4394e4
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-10-31 14:17:33 +0800

    Update ClashRoyale PWA

---
+++ ClashRoyale/README.md
@@ -0,0 +1,2 @@
+# LLaMA-Clash-Royale
+Development of the project started on 2023/10/31, led by Justin Hsu.
---
+++ ClashRoyale/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'llama-clashroyale-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/js/canvas.js',
+    '/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(assetsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
