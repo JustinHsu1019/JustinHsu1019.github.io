d522394cebd04f038b6a4e72f80c74d2049e09aa
Justin.Hsu <141555665+JustinHsu1019@users.noreply.github.com>
2024-01-29 14:27:09 +0800

    Update manage page

---
+++ login/manage.js
@@ -0,0 +1,72 @@
+const apiUrl = 'https://justincode.pythonanywhere.com';
+
+function getHeaders() {
+    const token = localStorage.getItem('jwtToken');
+    return {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    };
+}
+
+function readUser() {
+    const email = $('#email').val();
+    $.ajax({
+        url: `${apiUrl}/read_member/${email}`,
+        method: 'GET',
+        headers: getHeaders(),
+        success: function(response) {
+            $('#name').val(response.name);
+            $('#role').val(response.role);
+            $('#gender').val(response.gender);
+            $('#instagram').val(response.instagram);
+        },
+        error: function(xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+function updateUser() {
+    const email = $('#email').val();
+    const userData = {
+        name: $('#name').val(),
+        role: $('#role').val(),
+        gender: $('#gender').val(),
+        instagram: $('#instagram').val()
+    };
+
+    $.ajax({
+        url: `${apiUrl}/update_member/${email}`,
+        method: 'PUT',
+        headers: getHeaders(),
+        data: JSON.stringify(userData),
+        success: function(response) {
+            alert(response.message);
+        },
+        error: function(xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+function deleteUser() {
+    const email = $('#email').val();
+    $.ajax({
+        url: `${apiUrl}/delete_member/${email}`,
+        method: 'DELETE',
+        headers: getHeaders(),
+        success: function(response) {
+            alert(response.message);
+        },
+        error: function(xhr, status, error) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+$(document).ready(function() {
+    var userEmail = localStorage.getItem('userEmail');
+    if (userEmail) {
+        $('#email').val(userEmail);
+    }
+});
